fa862819e20516df2b41bb6aee076ec236be6276
lizz <lizz@goktech.cn>
2020-02-25 14:06:16 +0800

    feat:修改了左边侧边栏

--- src/containers/menu.js
+++ src/containers/menu.js
@@ -1,26 +1,38 @@
 const menu = [
+    // {
+    //     key: '/index',
+    //     title: '首页',
+    //     icon: 'home',
+    //     auth: [1]
+    // },
     {
-        key: '/index',
-        title: '首页',
-        icon: 'home',
-        auth: [1]
-    },
-    {
-        title: '表单',
-        key: '/form',
-        icon: 'form',
+        title: '项目管理',
+        key: '/projectManage',
+        icon: 'folder',
         subs: [
-            { title: '基础表单', key: '/form/base-form', icon: '' },
+            { title: '项目列表管理', key: '/projectManage/projectList', icon: '' },
+            { title: '项目人员管理', key: '/form/base-form', icon: '' },
+            { title: '项目任务管理', key: '/form/base-form', icon: '' }
         ]
     },
     {
-        title: '展示',
+        title: '任务中心',
         key: '/show',
-        icon: 'pie-chart',
-        subs: [
-            { title: '表格', key: '/show/table', icon: '' },
-        ]
+        icon: 'form',
+        subs: [{ title: '表格', key: '/show/table', icon: '' }]
     },
+    {
+        title: '报表中心',
+        key: '/index',
+        icon: 'bar-chart',
+        subs: [{ title: '表格', key: '/index', icon: '' }]
+    },
+    {
+        title: '系统管理',
+        key: '/index',
+        icon: 'setting',
+        subs: [{ title: '表格', key: '/show/table', icon: '' }]
+    }
 ]
 
 export default menu
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,10 +8,14 @@ const FormBaseView = loadable(() => import(/* webpackChunkName: 'formBase' */ '@
 // 展示
 const TableView = loadable(() => import(/* webpackChunkName: 'table' */ '@/views/ShowView/Table'))
 
+// 项目管理
+const ProjectManange = loadable(() => import('@/views/ProjectManage/ProjectList')) //项目列表管理
+
 const routes = [
     { path: '/index', exact: true, name: 'Index', component: Index, auth: [1] },
     { path: '/form/base-form', exact: false, name: '表单', component: FormBaseView },
     { path: '/show/table', exact: false, name: '表格', component: TableView },
+    { path: '/projectManage/projectList', exact: false, name: '项目列表管理', component: ProjectManange }
 ]
 
 export default routes
